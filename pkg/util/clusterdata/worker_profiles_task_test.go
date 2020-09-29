7f5c535dcd5c4fa4ea5cc6c07081fd06d8ad5cce
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2020-09-29 10:10:48 +0100

    move to go.mod, add deps.go, update vendor

--- pkg/util/clusterdata/worker_profiles_task_test.go
+++ pkg/util/clusterdata/worker_profiles_task_test.go
@@ -10,8 +10,8 @@ import (
 	"testing"
 
 	"github.com/Azure/go-autorest/autorest/to"
-	machinev1beta1 "github.com/openshift/cluster-api/pkg/apis/machine/v1beta1"
-	clusterapi "github.com/openshift/cluster-api/pkg/client/clientset_generated/clientset"
+	machinev1beta1 "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
+	clusterapi "github.com/openshift/machine-api-operator/pkg/generated/clientset/versioned"
 	"github.com/openshift/cluster-api/pkg/client/clientset_generated/clientset/fake"
 	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
